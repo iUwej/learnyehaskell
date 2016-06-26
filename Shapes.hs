1d81da5f4376721d31ff448fc76b05ddb68c2064
iUwej <wawerumunene@gmail.com>
2016-06-26 13:39:43 +0300

    moved the Shapes function into a module

--- Shapes.hs
+++ Shapes.hs
@@ -1,3 +1,13 @@
+module Shapes
+(	Point(..)
+	, Shape(..)
+	,surface
+	,nudge
+	,baseCircle
+	,baseRect
+
+
+) where 
 data Point = Point Float Float deriving (Show)
 
 data Shape = Circle Point  Float | Rectangle Point Point deriving (Show)
